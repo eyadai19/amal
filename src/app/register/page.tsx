12b1b8888eef59d07a6ce9b05b3aaeedb2961dfc
eyadai19 <eyad.sy19@icloud.com>
2025-03-20 16:07:49 +0300

    eyad

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 import RegisterForm from "@/components/RegisterForm";
 import { lucia } from "@/lib/auth";
 import { db } from "@/lib/db";
-import { TB_user } from "@/lib/schema";
+import { TB_user, TB_user_t3 } from "@/lib/schema";
 import { RegisterFormError, registerFormSchema } from "@/lib/types/authSchemas";
 import hash from "@/lib/utils";
 import { eq } from "drizzle-orm";
@@ -48,6 +48,31 @@ export async function RegisterAction(
 				);
 				return { field: "photo", message: "Failed to upload profile photo" };
 			}
+		}
+
+		const alphaBitStageIndex = await db.query.TB_alphaBit_level.findFirst({
+			where: (stage, { eq }) => eq(stage.index, 0),
+		});
+
+		if (!alphaBitStageIndex) return;
+
+		const digitStageIndex = await db.query.TB_digit_level.findFirst({
+			where: (stage, { eq }) => eq(stage.index, 0),
+		});
+
+		if (!digitStageIndex) return;
+
+		const newUserT3 = {
+			id: nanoid(),
+			userId: newUser.id,
+			alphaBitId: alphaBitStageIndex.id,
+			digitId: digitStageIndex.id,
+		};
+
+		try {
+			await db.insert(TB_user_t3).values(newUserT3);
+		} catch {
+			return { field: "root", message: "" };
 		}
 
 		const session = await lucia.createSession(newUser.id, {});
