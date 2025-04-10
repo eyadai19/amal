f87e357fc228095a1477c1fbdc93121a2e3e0fb1
eyadai19 <eyad.sy19@icloud.com>
2025-04-10 16:18:59 +0300

    edit url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,4 +3,4 @@ export const numberOcrApi = `${baseUrl}/predictNumber`;
 export const alphaBitOcrApi = `${baseUrl}/predictAlphabit`;
 
 const baseNgrokUrl = "https://47f0-34-57-86-248.ngrok-free.app";
-export const legalSearchApi = `${baseNgrokUrl}/api/similar_questions`;
+export const legalSearchApi = `${baseNgrokUrl}/similar_questions`;
